d5f9fb65446ad54a9be1496129f885fae55bdf12
Noel Mendoza <nmendozavd@gmail.com>
2019-05-08 05:44:59 -0700

    udpated with readme

--- comparelists.py
+++ comparelists.py
@@ -1,5 +1,8 @@
-def compare_lists(list_one,list_two):
+
+def compare_lists(list_one, list_two):
     if list_one == list_two:
         print "The lists are the same"
     else:
-        print "The lists are not the same"
+        print "The list are not the same"
+
+print compare_lists(list_one = [1, 2, 5, 6, 2],list_two = [2, 2, 5, 6, 2])
